14a07e7c1f1ac23866b08d3936ea0e939a002a71
kinshukjainn <kinshuk25jan04@gmail.com>
2024-09-11 14:03:32 +0530

    commits done pls check

--- linkedlist/LinkedlistCollection.java
+++ linkedlist/LinkedlistCollection.java
@@ -1,9 +1,22 @@
 package linkedlist;
-
+import java.util.*;
 public class LinkedlistCollection {
 
     public static void main(String[] args) {
+
+
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.push(2);
+        ll.push(3);
+        ll.push(4);
+        ll.push(5);
+        ll.push(6);
+
+        System.out.println(ll);
         
+
+        // linkelist through collection framework : 
+
     }
     
 }
