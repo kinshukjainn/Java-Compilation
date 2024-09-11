224aebfcecd54f3a915354c3e91eaafea94ef6b7
kinshukjainn <kinshuk25jan04@gmail.com>
2024-09-11 12:57:23 +0530

    second commit changes in linkedlistintro merging

--- linkedlist/Linkedlistintro.java
+++ linkedlist/Linkedlistintro.java
@@ -1,6 +1,6 @@
 package linkedlist;
 
-import java.utils*;
+import java.util.*;
 public class Linkedlistintro {
 
     public static void main(String[] args) {
@@ -8,12 +8,12 @@ public class Linkedlistintro {
 
         // Linkedlist 
 
-        Linkedlist<Integet> ll = new Linkedlist<>()
-        ll.push(9)
-        ll.push(10)
-        ll.push(11)
-        ll.push(12)
-        ll.push(13)
+        LinkedList<Integer> ll = new LinkedList<>();
+        ll.push(9);
+        ll.push(10);
+        ll.push(11);
+        ll.push(12);
+        ll.push(13);
 
         System.out.print(ll);
         
